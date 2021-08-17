fd74d10e72b6262503c3c307886e7d530387ad5e
ShivamSharma3589 <85331338+ShivamSharma3589@users.noreply.github.com>
2021-08-18 00:17:07 +0530

    new updates

--- js.js
+++ js.js
@@ -1,51 +1,93 @@
-let person;      
-let cmove, cScore = 0, pScore = 0;
-let myAudio = document.getElementById('myAudio');
+let person = 'player';
+let cmove;
+let pScore = 3;
+let cScore = 0;
+let chance = 0;
 let newGame = document.getElementById('newGame');
 let pape = document.getElementById('paper');
 let scisso = document.getElementById('scissors');
 let roc = document.getElementById('rock');
 let winn = document.getElementById("winner");
-let playerScore = document.getElementById("playerScore");
-let computerScore = document.getElementById("computerScore");
 let name = document.getElementById("name");
 let controls = document.getElementById("controls");
-let computerImage = document.getElementById("computerImage");
 let myImage = document.getElementById('myImage');
 let sound = document.getElementById("sound");
+let star = document.getElementsByClassName('star');
+let fstars = document.getElementsByClassName('fstars');
+let sweetAlert = document.getElementById('sweetAlert');
+let sweet = document.getElementById('sweet');
+let winnerAlert = document.getElementById('winnerAlert');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+
 // sound adjustments 
-window.onload=()=>{
-    myAudio.volume=0.5;
+window.onload = () => {
+    sweet.style.transform = "scale(1)";
+}
+// to announce the winner 
+function announcement() {
+    let Alert = document.getElementById('Alert');
+    let result = document.getElementById('result');
+    if (chance == 3) {
+        winnerAlert.style.display = 'flex';
+        Alert.style.transform = 'scale(1)';
+        result.innerHTML = "none";
+        if (cScore >= 2) {
+            result.innerHTML = "Computer";
+            for (let i = 0; i < cScore; i++) {
+                fstars[i].className = 'fas fa-star fstars';
+                fstars[i].style.color = 'gold';
+            }
+        } else {
+            result.innerHTML = person;
+            for (let i = 0; i < pScore-3; i++) {
+                fstars[i].className = 'fas fa-star fstars';
+                fstars[i].style.color = 'gold';
+            }
+        }
+    }
 }
 // function to get the user name is here.
 function myFunction() {
-    person = prompt("Please enter your name", "Player");
-    cScore = 0;
-    pScore = 0;
+    person = prompt("Please enter your name", person);
     winn.innerHTML = "Winner...?";
-    playerScore.innerHTML = pScore;
-    computerScore.innerHTML = cScore;
     if (person != null) {
         name.innerHTML = person;
         controls.style.display = "block";
-        sound.style.display="inline";
+        sound.style.display = "inline";
         myAudio.play();
-    }else{
-        name.innerHTML="Player";
+    } else {
+        name.innerHTML = "Player";
     }
     if (person == "") {
         person = "Player";
         name.innerHTML = person;
+    }
+    Reset();
+}
+// funtion for reset the game 
+function Reset() {
+    pScore = 3;
+    cScore = 0;
+    chance = 0;
+    let len = star.length;
+    for (let i = 0; i < len; i++) {
+        star[i].className = 'far fa-star star';
+        star[i].style.color = 'rgba(0, 0, 0, 0.5)';
+    }
+    for (let i = 0; i < 3; i++) {
+        fstars[i].className = 'far fa-star fstars';
+        fstars[i].style.color = 'white';
     }
 }
 // here is the function to get the move from the computer
 function computerMove() {
     return Math.floor((Math.random() * 3) + 1);
 }
-
 // here images will appear chosen by computer
 function computerimg() {
     cmove = computerMove();
+    let computerImage = document.getElementById("computerImage");
     if (cmove == 1) {
         computerImage.src = 'paper.gif';
     } else
@@ -56,11 +98,13 @@ function computerimg() {
                 computerImage.src = 'rock.gif';
             }
 }
-// function for deciding the winner 
+// function for deciding the winner for player uses
 function subWinner() {
     winn.innerHTML = ("Winner : " + person);
-    pScore += 1;
-    playerScore.innerHTML = pScore;
+    star[pScore].className = 'fas fa-star star';
+    star[pScore].style.color = 'gold';
+    pScore++;
+    chance++;
 }
 // function for deciding the winner 
 function winner(player) {
@@ -80,10 +124,16 @@ function winner(player) {
                     winn.innerHTML = draw;
                 } else {
                     winn.innerHTML = com;
-                    cScore += 1;
-                    computerScore.innerHTML = cScore;
+                    star[cScore].className = "fas fa-star star";
+                    star[cScore].style.color = 'gold';
+                    cScore++;
+                    chance++;
                 }
             }
+    if (chance == 3) {
+        controls.style.display = 'none';
+    }
+    announcement();
 }
 // function to show paper
 function paper() {
@@ -91,13 +141,13 @@ function paper() {
     computerimg();
     winner(1);
 }
-// function to show paper
+// function to show scissors
 function scissor() {
     myImage.src = 'scissor.gif';
     computerimg();
     winner(2);
 }
-// function to show paper
+// function to show rock
 function rock() {
     myImage.src = 'rock.gif';
     computerimg();
@@ -107,14 +157,25 @@ newGame.addEventListener('click', myFunction);
 pape.addEventListener('click', paper);
 scisso.addEventListener('click', scissor);
 roc.addEventListener('click', rock);
-sound.addEventListener('click',()=>{
+// sound code goes here 
+sound.addEventListener('click', () => {
+
+    let myAudio = document.getElementById('myAudio');
     if (myAudio.muted) {
-        sound.className="fa-solid fa-volume";
-        myAudio.muted=false;
-        sound.style.color="white";
+        sound.className = "fa-solid fa-volume";
+        myAudio.muted = false;
+        sound.style.color = "white";
     } else {
-        sound.className="fa-solid fa-volume-xmark";
-        myAudio.muted=true;
-        sound.style.color="red";
+        sound.className = "fa-solid fa-volume-xmark";
+        myAudio.muted = true;
+        sound.style.color = "red";
     }
 });
+// sweet alert button code goes here
+btn1.addEventListener('click', () => {
+    sweetAlert.style.display = 'none';
+});
+// winner alert code goes here 
+btn2.addEventListener('click', () => {
+    winnerAlert.style.display = 'none';
+})
